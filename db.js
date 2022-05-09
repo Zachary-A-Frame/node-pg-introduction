a3f266275ab5f7a3c2140ed092d9986a6f864b93
Zach <Zacaframe@gmail.com>
2022-05-08 23:14:24 -0400

    Second commit

--- db.js
+++ db.js
@@ -1,2 +1,25 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+let DB_URI;
+
+// If we're running in test "mode", use our test db
+// Make sure to create both databases!
+// postgresql://postgres:{password}@localhost:5432/blogly
+// if (process.env.NODE_ENV === "test") {
+//     DB_URI = "postgresql://postgres:1234@localhost:5432/usersdb_test";
+// } else {
+//     DB_URI = "postgresql://postgres:1234@localhost:5432/usersdb";
+// }
+
+DB_URI = "postgresql://postgres:1234@localhost:5432/biztime";
+
+
+let db = new Client({
+    connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
